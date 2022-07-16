3764fb303c247a9087ee3ff4c8553b5532109d8b
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-07-17 01:15:17 +0500

    Bootcamp & User Relationship and Bootcamp Ownership

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' })
 // Load models
 const Bootcamp = require('./models/Bootcamp')
 const Course = require('./models/Course')
+const User = require('./models/User')
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -22,11 +23,16 @@ const courses = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 )
 
+const users = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+)
+
 // Import into DB
 const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps)
 		await Course.create(courses)
+		await User.create(users)
 
 		console.log('Data Imported...'.green.inverse)
 		process.exit()
@@ -40,6 +46,7 @@ const deleteData = async () => {
 	try {
 		await Bootcamp.deleteMany()
 		await Course.deleteMany()
+		await User.deleteMany()
 
 		console.log('Data Destroyed...'.red.inverse)
 		process.exit()
